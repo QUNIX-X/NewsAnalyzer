a4b6d578f675b1a6bef3e7ad8029b261243452aa
QUNIX-X <danil07042000@yandex.ru>
2020-07-08 16:25:38 +0500

    Replacing the News API with the Google News API
    The News API restricted access to open sources, so I had to replace it with the Google News API

--- src/modules/api/NewsApi.js
+++ src/modules/api/NewsApi.js
@@ -8,7 +8,7 @@ export default class NewsApi {
   }
 
   sendRequest(query) {
-    const url = `${this._url}q=${query}&language=ru&from=${this.weekAgoDate}&to=${this.nowDate}&sortBy=popularity&pageSize=100&apiKey=${this._apiKey}`;
+    const url = `${this._url}q=${query}&lang=ru&max=100&mindate=${this.weekAgoDate}$maxdate=${this.nowDate}&token=${this._apiKey}`; //NEWS API - `${this._url}q=${query}&language=ru&from=${this.weekAgoDate}&to=${this.nowDate}&sortBy=popularity&pageSize=100&apiKey=${this._apiKey}`
     return fetch(url)
       .then(status => {
         if (status.ok) {
